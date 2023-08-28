009f914267d23beea94bd374dbc7e51bc19fd7a6
Yannis <yannismadu777@gmail.com>
2023-08-28 19:14:07 +0100

    Design navbar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav className="flex text-white mx-10 my-5">
+      <p className="text-2xl text-cyan-500">Y</p>
+      <div className="ml-auto flex gap-14 font-extralight">
+        <a href="#home">
+          <span className="text-cyan-500">01.</span> Home
+        </a>
+        <a href="#about">
+          <span className="text-cyan-500">02.</span> About
+        </a>
+        <a href="#work">
+          <span className="text-cyan-500">03.</span> Work
+        </a>
+        <a href="#contact">
+          <span className="text-cyan-500">04.</span> Contact
+        </a>
+      </div>
+    </nav>
+  );
 }
