9d66b58696704af44d9d460ff3765fafd8826f88
Yannis <yannismadu777@gmail.com>
2023-08-29 15:15:31 +0100

    Make contact component responsive

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -5,10 +5,10 @@ export default function Contact() {
   return (
     <section
       id="contact"
-      className="justify-center flex mt-sectionGap mb-contactGap -ml-40"
+      className="justify-center flex mt-sectionGap mb-contactGap xl:pr-56"
     >
       <div className="flex flex-col gap-6">
-        <h2 className="text-sm text-cyan-500">{`04. What's Next?`}</h2>
+        <h2 className="text-sm text-cyan-500 ">{`04. What's Next?`}</h2>
         <p className="text-4xl text-neutral-300 font-bold">{`Don't Be Shy`}</p>
         <p className="text-neutral-400">
           {`My inbox is open. I'm always happy to answer a question or chat about
